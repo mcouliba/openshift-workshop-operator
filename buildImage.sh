9ec8e06b80783667af934809219b9cb5d00bcc9f
mcouliba <mcouliba@redhat.com>
2020-03-19 16:02:51 +0100

    Cleanup master

--- buildImage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-IMAGE=$1
-
-operator-sdk build ${IMAGE}
-docker push ${IMAGE}
--- pkg/apis/openshift/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the openshift v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=openshift.redhat.com
-package v1alpha1
